085448e7ee8cc8fa3d66579e64d938b08aef9f2f
joseucarvajal <joseucarvajal@gmail.com>
2021-04-02 17:03:19 -0500

    v1.0 mensajes y fixes varios

--- src/Pages/Home/Home.page.jsx
+++ src/Pages/Home/Home.page.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            Home page
+            Home page 
+            <br/>
+            <b>v1.0</b>: List, add, update TODOS: lógica en componentes
         </div>
     )
 }
